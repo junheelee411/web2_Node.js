93dd89c5f137bcc15c936f31c4838e656b4e2ca9
junhee lee <80439077+junheelee411@users.noreply.github.com>
2023-09-08 14:18:06 +0900

    URL - 비교 연산자

--- syntax/template.js
+++ syntax/template.js
@@ -3,7 +3,7 @@ var me = 'Junhee-Lee';
 //String literals
 var letter = 'Dear You' + '\n Hi my name is ' + me +
 '\n Nice to meet you!!';
-
+d
 //Template literals
 var letter = `Dear You
 Hi my name is 
